4dee2420c5475e0bf77e0aa8389b3f1fd8826684
JTryon <JTryon@n11616.americas.quiksilver.corp>
2014-11-06 16:05:17 -0800

    cleanup

--- src/main/java/com/qs/services/service/impl/SeasonServiceImpl.java
+++ src/main/java/com/qs/services/service/impl/SeasonServiceImpl.java
@@ -39,59 +39,11 @@ public class SeasonServiceImpl implements SeasonService {
 	@Autowired
 	private SeasonSao sao ;
 
-	/*
-	 * 
-	 * How to get the seasons 
-	 * 
-	 * 1) Use the salesRepId from the sales rep profile to call SeasonSao.getRepPrebkSeasons(...)
-	 * 
-	 * This will get a list of prebook seasons for the sales rep
-	 * 
-	 * 2) Use the resulting list from (1) to call SeasonSao.getActiveSeasonProducts(...)
-	 * 
-	 * This will get a list of products for the seasons for the sales rep
-	 * 
-	 * 3) Use the resulting list from (1) to call SeasonDao.getSeason(...) with the sales_org, season_id and collection
-	 * 
-	 * This will get detail information about that season from the connect database
-	 * 
-	 * 
-	 */
 	@Override
 	public SAPPrebookSeasonList getSeasons(String salesRepId) throws JsonGenerationException, 
 								JsonMappingException, IOException {
 		SAPPrebookSeasonList prebookSeasons = sao.getRepPrebkSeasons(salesRepId);
 		return prebookSeasons ;
-//		SAPActiveSeasonProductList activeSeasonProducts = sao.getActiveSeasonProducts(salesRepId, prebookSeasons);
-//		
-//		if(activeSeasonProducts != null && activeSeasonProducts.getProducts() != null){
-//			Map <String, String> imageUrls = productDao.getMediumHeroImageUrls(activeSeasonProducts.getProducts()) ;
-//			String url = null ;
-//			for(int i=0; i<activeSeasonProducts.getProducts().size(); i++){
-//				url = imageUrls.get(activeSeasonProducts.getProducts().get(i).getProduct()) ;
-//				logger.info("Adding (" + url + ") to (" + activeSeasonProducts.getProducts().get(i).getProduct() + ")");
-//				activeSeasonProducts.getProducts().get(i).setImageUrl(url);
-//			}
-//		}
-//
-//		for (SAPPrebookSeason prebookSeason : prebookSeasons.getPrebookSeasons()) {
-//			try {
-//				Season season = dao.getSeason(prebookSeason.getSalesOrg(), prebookSeason.getSeason(), 
-//						prebookSeason.getCollection());
-//				activeSeasonProducts.getSeasons().add(season);
-//			} catch (IllegalStateException ise) {
-//				logger.error("Failed to get season, salesOrg: " + prebookSeason.getSalesOrg()
-//						+ ", season: " +  prebookSeason.getSeason() 
-//						+ ", collection: " + prebookSeason.getCollection(), ise);
-//			}
-//		}
-//		
-//		// Blank out the product lists for testing
-//		activeSeasonProducts.setProducts(new ArrayList<Product>()); ;
-//		activeSeasonProducts.setProductPrices(new ArrayList<ProductPrice>());
-//		activeSeasonProducts.setProductSizes(new ArrayList<ProductSize>());
-//		
-//		return activeSeasonProducts;
 	}
 
 	@Override
